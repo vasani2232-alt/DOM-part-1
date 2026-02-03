5a262eb548e416b8162b1c422471594f9fdd420b
vasani2232-alt <vasani2232@.in>
2026-02-03 20:23:47 +0530

    Complete DOM part-1

--- script.js
+++ script.js
@@ -13,8 +13,30 @@
 // let parahs = document.getElementsByTagName("p");
 // console.dir(parahs);
 
-let firstEl = document.querySelector("#myId"); //.used to class and # is used to Id
-console.dir(firstEl);
+// let firstEl = document.querySelector("p"); //.used to class and # is used to Id
+// console.dir(firstEl);
 
 // let allEl = document.querySelectorAll(".myClass"); //all elements
 // console.dir(allEl);
+
+// console.dir(document.body.firstChild);
+
+// let div = document.querySelector("div");
+// console.dir(div);
+
+// let heading = document.querySelector("h1");
+
+//Practice que1
+// let h2 = document.querySelector("h2");
+// console.dir(h2.innerText);
+
+// h2.innerText = h2.innerText + " from apna college students"; //concatenate
+
+let divs = document.querySelectorAll(".box");
+
+let idx = 1;
+for(div of divs){
+    div.innerText = `new unique value ${idx}`;
+    idx++;
+}
+    

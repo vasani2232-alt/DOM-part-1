261b3c2ae79693fc59abd8a6aebc6194fc1c68ce
vasani2232-alt <vasani2232@.in>
2026-02-03 11:28:15 +0530

    update DOM

---
+++ script.js
@@ -0,0 +1,20 @@
+// console.log("hello!");
+// alert("Hello world!")
+// console.dir(document.body.childNodes[1]);
+// document.body.childNodes[3].innerText = "abcd";
+// console.dir(document.body); 
+//  let button = document.getElementById("myId"); //h1
+//  console.dir(button);
+
+// let headings = document.getElementsByClassName("myClass");
+// console.dir(headings);
+// console.log(headings);
+
+// let parahs = document.getElementsByTagName("p");
+// console.dir(parahs);
+
+let firstEl = document.querySelector("#myId"); //.used to class and # is used to Id
+console.dir(firstEl);
+
+// let allEl = document.querySelectorAll(".myClass"); //all elements
+// console.dir(allEl);
